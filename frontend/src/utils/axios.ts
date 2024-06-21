5dbf7308773fd7666a62d68ad46decc4db8a4d8c
prajwalg19 <theimperical8@gmail.com>
2024-06-21 22:42:02 +0530

    changes

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     withCredentials: true,
-    baseURL: "https://project-blog-black.vercel.app/"
+    baseURL: "https://project-blog-black.vercel.app"
 })
 
 export default axiosInstance;
