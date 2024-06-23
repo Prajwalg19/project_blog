14b215f7bb8a24a89b5b2f97b01ab5da99e9b5bc
prajwalg19 <theimperical8@gmail.com>
2024-06-24 00:40:26 +0530

    change backend url for production

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     withCredentials: true,
-    //baseURL: "https://project-blog-black.vercel.app"
-    baseURL: "http://localhost:4000"
+    baseURL: "https://project-blog-black.vercel.app"
+    //baseURL: "http://localhost:4000"
 })
 
 export default axiosInstance;
